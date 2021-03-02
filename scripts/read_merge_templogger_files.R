8cf1811978231ae0edd7dcaf4af69a04bf6a61d2
jebyrnes <jarrett.byrnes@umb.edu>
2021-03-02 13:06:19 -0500

    Added a line where I bound the seprate data files together.

--- scripts/read_merge_templogger_files.R
+++ scripts/read_merge_templogger_files.R
@@ -7,7 +7,11 @@ library(dplyr)
 library(purrr)
 setwd(here::here())
 
-#what are the 
+#what are the files we want to use
 files <- list.files("logger_info/", full.names = TRUE)
 
+#read in files
 all_files <- map(files, read_excel)
+
+#bind this into a data frame
+all_files <- bind_rows(all_files)
